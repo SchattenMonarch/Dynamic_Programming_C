88c0b4d03e5d59b3d23734c6e4a193638688df51
Dennis Folz <folzdennis@gmail.com>
2022-08-26 14:55:53 +0200

    implemented test cases for fibonacci in main

--- dynamic_programming_c/main.c
+++ dynamic_programming_c/main.c
@@ -1,42 +1,26 @@
 #include <stdio.h>
+#include <assert.h>
 
 #include "hash.h"
+#include "fibonacci.h"
 
 int main()
 {
-    hash_set_t* set = init_hash_set();
-
-    int v1 = 10, v2 = 20, v3 = 30, v4 = 40, v5 = 50, v6 = 60, v7 = 70;
-
-    printf("Value %d was add ? %d\n", v1, add(set, &v1));
-    printf("Value %d was add ? %d\n", v1, add(set, &v1));
-    printf("contains %d ? %d\n", v1, contains(set, &v1));
-
-    printf("Value %d was add ? %d\n", v2, add(set, &v2));
-    printf("Value %d was add ? %d\n", v2, add(set, &v2));
-    printf("contains %d ? %d\n", v2, contains(set, &v2));
-
-    printf("Value %d was add ? %d\n", v3, add(set, &v3));
-    printf("Value %d is add ? %d\n", v3, add(set, &v3));
-    printf("contains %d ? %d\n", v3, contains(set, &v3));
-
-    printf("Value %d was add ? %d\n", v4, add(set, &v4));
-    printf("Value %d was add ? %d\n", v4, add(set, &v4));
-    printf("contains %d ? %d\n", v4, contains(set, &v4));
-
-    printf("Value %d was add ? %d\n", v5, add(set, &v5));
-    printf("Value %d was add ? %d\n", v5, add(set, &v5));
-    printf("contains %d ? %d\n", v5, contains(set, &v5));
-
-    printf("Value %d is add ? %d\n", v6, add(set, &v6));
-    printf("Value %d is add ? %d\n", v6, add(set, &v6));
-    printf("contains %d ? %d\n", v6, contains(set, &v6));
-
-    printf("contains %d ? %d\n", v7, contains(set, &v7));
-
-    delete_hash(set, &v6);
-
-    printf("contains %d ? %d\n", v6, contains(set, &v6));
-
+    assert(fib(1) == 1);
+    assert(fib(2) == 1);
+    assert(fib(3) == 2);
+    assert(fib(4) == 3);
+    assert(fib(5) == 5);
+    assert(fib(6) == 8);
+    assert(fib(7) == 13);
+    assert(fib(8) == 21);
+    assert(fib(9) == 34);
+    assert(fib(10) == 55);
+    assert(fib(11) == 89);
+    assert(fib(12) == 144);
+    assert(fib(13) == 233);
+    assert(fib(20) == 6765);
+    assert(fib(25) == 75025);
+    puts("all tests passed");
     return 0;
 }
