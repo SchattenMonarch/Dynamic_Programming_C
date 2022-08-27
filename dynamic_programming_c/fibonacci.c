4fac9595a3c6c1199e2ec71ed7fef0588c8f443d
Dennis Folz <folzdennis@gmail.com>
2022-08-27 15:31:33 +0200

    fixed the bug. LESSON LEARNED: Don't call kh_put before a recursive function, or too many same keys will be stored without a value. This leads to an undefined kh_value(map_ptr, it), because kh_get(map, map_ptr, n) will return an iterator to a key without a value!

--- dynamic_programming_c/fibonacci.c
+++ dynamic_programming_c/fibonacci.c
@@ -2,37 +2,42 @@
 #include <stdbool.h>
 #include <assert.h>
 
-KHASH_MAP_INIT_INT(map, unsigned long long)
+KHASH_MAP_INIT_INT(map, long long)
 khash_t(map)* map_ptr;
+int ret;
+long long val;
+bool is_missing;
+khiter_t it;
 
-unsigned long long fib(int n) {
-	int ret;
-    bool is_missing;
-	static bool map_initiated = false;
-	khiter_t it;
-	if (!map_initiated) {
-		map_ptr = kh_init(map);
-		map_initiated = true;
+void init_fib(void) {
+	map_ptr = kh_init(map);
+	is_missing = true;
+	ret = 0;
+	val = 0;
+	it = -1;
+}
+
+long long fib(int n) {
+	it = kh_get(map, map_ptr, n);
+
+	if (it != kh_end(map_ptr)){
+		val = kh_value(map_ptr, it);
+//		printf("Key #%d exists. Value: %lld\n", n, val);
+		return val;
 	}
-	
-	printf("Entered fib with n = %d\n", n);
 	if (n <= 2) return 1;
-	it = kh_get(map, map_ptr, n);
-	
-	is_missing = (it == kh_end(map_ptr));
 
-	if (is_missing) {
-		it = kh_put(map, map_ptr, n, &ret);
-		printf("CALCULATING FIB FOR n = %d\n", n);
-		kh_value(map_ptr, it) = fib(n - 1) + fib(n - 2);
-	}
-	else {
-		printf("VALUE FOR n = %d already exists: %lld\n", kh_value(map_ptr, it));
-		return kh_value(map_ptr, it);
-	}
-	it = kh_get(map, map_ptr, n);
-	is_missing = (it == kh_end(map_ptr));
-	assert(is_missing == false);
-	return kh_value(map_ptr, it);
+	val = fib(n - 1) + fib(n - 2);
+	//LESSON LEARNED: Don't call kh_put before a recursive function, or too many same keys will be stored without a value.
+	//This leads to an undefined kh_value(map_ptr, it), because kh_get(map, map_ptr, n) will return an iterator to a key without a value!
+	it = kh_put(map, map_ptr, n, &ret); 
+//	printf("CALCULATED: Key #%d \t Value: %lld\n", n, val);
+	kh_value(map_ptr, it) = val;
+
+	return val;
+}
+
+void deinit_fib(void) {
+	free(map_ptr);
 }
 
