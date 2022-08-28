ada2afe277086c0652aa3b43cbd29a7e32ded302
Dennis Folz <folzdennis@gmail.com>
2022-08-28 02:12:57 +0200

    created grid-traveler.h and added function declarations

---
+++ dynamic_programming_c/grid_traveler.h
@@ -0,0 +1,10 @@
+#pragma once
+#ifndef __GRID_TRAVELER.H__
+#define __GRID_TRAVELER.H__
+
+void init_grid_traveler(void);
+long long grid_traveler(int n);
+void deinit_grid_traveler(void);
+
+#endif // !__GRID_TRAVELER.H__
+
