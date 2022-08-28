6420de909e08fccd3d322dd45395f8cfd16a0119
Dennis Folz <folzdennis@gmail.com>
2022-08-28 02:30:11 +0200

    fixed error by removing '.' in the makro

--- dynamic_programming_c/grid_traveler.h
+++ dynamic_programming_c/grid_traveler.h
@@ -1,10 +1,10 @@
 #pragma once
-#ifndef __GRID_TRAVELER.H__
-#define __GRID_TRAVELER.H__
+#ifndef __GRID_TRAVELER__
+#define __GRID_TRAVELER__
 
 void init_grid_traveler(void);
 long long grid_traveler(int row, int column);
 void deinit_grid_traveler(void);
 
-#endif // !__GRID_TRAVELER.H__
+#endif // !__GRID_TRAVELER__
 
