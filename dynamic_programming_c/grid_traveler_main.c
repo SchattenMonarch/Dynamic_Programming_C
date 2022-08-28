6089378a6489875d3b9174f145963395e639c9bd
Dennis Folz <folzdennis@gmail.com>
2022-08-28 02:06:51 +0200

    Created grid_traveler_main.c and added a program description and author information

---
+++ dynamic_programming_c/grid_traveler_main.c
@@ -0,0 +1,10 @@
+/*
+author: Dennis Folz
+github profile : https://github.com/SchattenMonarch
+date:   28.08.2022
+last update: 28.08.2022
+problem: 
+Implement a grid-traveler using recursion.
+The function shall return the number of possible ways to travel through a n x m grid from the top left corner to the bottom right corner.
+It is only allowed to move down and right
+*/
