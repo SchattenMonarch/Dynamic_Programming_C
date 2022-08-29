633ec1f0ccb86b54a516683a6c3703068f0d6921
Dennis Folz <folzdennis@gmail.com>
2022-08-29 19:40:03 +0200

    added argument length to the can_sum function.

--- dynamic_programming_c/can_sum/can_sum.h
+++ dynamic_programming_c/can_sum/can_sum.h
@@ -3,6 +3,6 @@
 #define __CAN_SUM_H__
 #include <stdbool.h>
 
-bool can_sum(int target_value, const int numbers[]);
+bool can_sum(int target_value, const int numbers[], int length);
 
 #endif // !__CAN_SUM_H__
