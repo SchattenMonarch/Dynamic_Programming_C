7517a6c0feff20fcc10463e905ecc33294a537a7
Dennis Folz <folzdennis@gmail.com>
2022-08-29 22:04:50 +0200

    added new function declarations

--- dynamic_programming_c/can_sum/can_sum.h
+++ dynamic_programming_c/can_sum/can_sum.h
@@ -3,6 +3,8 @@
 #define __CAN_SUM_H__
 #include <stdbool.h>
 
+void init_can_sum(void);
 bool can_sum(int target_value, const int numbers[], int length);
+void deinit_can_sum(void);
 
 #endif // !__CAN_SUM_H__
