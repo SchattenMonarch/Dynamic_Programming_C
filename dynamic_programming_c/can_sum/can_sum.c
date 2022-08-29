52a67616b849deed4b1d80924f89e77b3915ffdd
Dennis Folz <folzdennis@gmail.com>
2022-08-29 19:41:32 +0200

    implemented the can_sum function. Tests passed

--- dynamic_programming_c/can_sum/can_sum.c
+++ dynamic_programming_c/can_sum/can_sum.c
@@ -1,1 +1,11 @@
 #include "can_sum.h"
+
+bool can_sum(int target_value, const int numbers[], int length) {
+	if (target_value == 0) return true;
+	if (target_value < 0) return false;
+
+	for (int i = 0; i < length; i++) {
+		if (can_sum(target_value - numbers[i], numbers, length)) return true;
+	}
+	return false;
+}
