76bac3986399076fd12b7da9d31b6c8557ad1c64
Dennis Folz <folzdennis@gmail.com>
2022-08-29 22:07:10 +0200

    implemented void deinit_can_sum(void)

--- dynamic_programming_c/can_sum/can_sum.c
+++ dynamic_programming_c/can_sum/can_sum.c
@@ -5,6 +5,10 @@ KHASH_MAP_INIT_INT(map, int)
 khash_t(map)* map_ptr;
 khiter_t it;
 
+void init_can_sum(void) {
+	map_ptr = kh_init(map);
+	it = -1;
+}
 
 bool can_sum(int target_value, const int numbers[], int length) {
 	if (target_value == 0) return true;
@@ -14,4 +18,8 @@ bool can_sum(int target_value, const int numbers[], int length) {
 		if (can_sum(target_value - numbers[i], numbers, length)) return true;
 	}
 	return false;
+}
+
+void deinit_can_sum(void) {
+	free(map_ptr);
 }
